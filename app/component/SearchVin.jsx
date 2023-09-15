c1d9ab91ca8c20b2d95a9102de5ace2c5348722d
superxp360 <kevinfernandez0416@gmail.com>
2023-09-15 14:46:45 -0400

    added the random VIN API and button

--- app/component/SearchVin.jsx
+++ app/component/SearchVin.jsx
@@ -1,21 +1,16 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import CarInfo from "./CarInfo";
-
 
 export default function SearchVin() {
   const [vin, setVin] = useState('');
   const [carData, setCarData] = useState(null);
 
-  const fetchData = async () => {
+  const fetchData = async (url) => {
     try {
-      const response = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/${vin}?format=json`);
+      const response = await fetch(url);
       const data = await response.json();
-      const { Make, ModelYear, Model } = data.Results[0];
-      console.log('Make:', Make);
-      console.log('Year:', ModelYear);
-      console.log('Model:', Model);
       setCarData(data);
     } catch (error) {
       console.error('Error:', error);
@@ -23,35 +18,52 @@ export default function SearchVin() {
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (vin) {
+      const url = `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/${vin}?format=json`;
+      fetchData(url);
+    }
+  }, [vin]);
 
   const handleButtonClick = () => {
     if (vin) {
-      fetchData();
+      const url = `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/${vin}?format=json`;
+      fetchData(url);
+    }
+  };
+
+  const handleRandomVinClick = async () => {
+    const url = "https://randomvin.com/getvin.php?type=real";
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+      const json = { VIN: data };
+      setVin(data); // Update the state with the random VIN
+      fetchData(url); // Fetch car information for the random VIN
+    } catch (error) {
+      console.error('Error:', error);
     }
   };
 
   return (
-    <>
-      <div className="flex justify-center w-full h-full ">
-        <div className="flex flex-col justify-center items-center mt-[70px] mx-auto border-2 border-black w-[300px] p-5 m-5 rounded-3xl bg-slate-50">
-          <div>
-            <h1 className="font-medium text-3xl text-center m-2 ">VIN Looker</h1>
-          </div>
+    <div className="flex justify-center w-full h-full">
+      <div className="flex flex-col justify-center items-center mt-[70px] mx-auto border-2 border-black w-[300px] p-5 m-5 rounded-3xl bg-slate-50">
+        <div>
+          <h1 className="font-medium text-3xl text-center m-2 ">VIN Looker</h1>
+        </div>
 
-          <div className="  rounded-3xl m-2 p-2">
-            <h2 className="text-md text-center">
-              A car's VIN (Vehicle Identification Number) is a unique code that serves as the vehicle's identification card. It contains information about the car's manufacturer, model, production year, and more. It's like a car's fingerprint and is used to track and identify vehicles by law enforcement, insurance companies, and others.
-            </h2>
-          </div>
+        <div className="rounded-3xl m-2 p-2">
+          <h2 className="text-md text-center">
+            A car's VIN (Vehicle Identification Number) is a unique code that serves as the vehicle's identification card. It contains information about the car's manufacturer, model, production year, and more. It's like a car's fingerprint and is used to track and identify vehicles by law enforcement, insurance companies, and others.
+          </h2>
+        </div>
 
-          <div>
-            <img className="m-2" src="./images/vin-decoder.avif" alt="VIN Decoder"></img>
-          </div>
+        <div>
+          <img className="m-2" src="./images/vin-decoder.avif" alt="VIN Decoder"></img>
+        </div>
 
-          <div className="flex justify-center w-[300px]">
-            <div className="flex flex-row border-2 border-gray-500 m-5 bg-white h-[50px] w-[400px] rounded-xl">
+        <div className="flex flex-col justify-center w-full p-2">
+          <div className="flex flex-row justify-center">
+            <div className="border-2 border-gray-500 bg-white h-[50px] w-full rounded-xl flex">
               <input
                 type="text"
                 placeholder="Enter VIN"
@@ -66,15 +78,17 @@ export default function SearchVin() {
                   }
                 }}
               />
-              <button
-                className="w-[20px] m-3"
-                onClick={handleButtonClick}
-              >
-                <img src="./images/searchIcon.png" alt="Search" className="w-[15px] h-[15px]" />
+              <button className="w-auto m-3" onClick={handleButtonClick}>
+                <img src="./images/searchIcon.png" alt="Search" className="w-[18px] h-[15px]" />
               </button>
             </div>
           </div>
-          <div className="m-2 w-full">
+          <p className="flex flex-row justify-end mr-2 m-1 text-xs" onClick={handleRandomVinClick}>
+            Try a VIN!
+          </p>
+        </div>
+
+        <div className="m-2 w-full">
           {carData && (
             <CarInfo
               make={carData.Results[0].Make}
@@ -83,8 +97,7 @@ export default function SearchVin() {
             />
           )}
         </div>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
